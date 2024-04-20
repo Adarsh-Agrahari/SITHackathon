0780bc8fc9c1bcec3aad9302c20a5d0cb7d16baa
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-04-20 11:37:14 +0530

    Extract handwritten text from pdf

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ load_dotenv()
 
 import streamlit as st
 import os
+import fitz  # PyMuPDF
+from PyPDF2 import PdfReader
+from PIL import Image
+import tempfile
 
 import google.generativeai as genai
-from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -18,20 +21,32 @@ def get_gemini_response(input,image):
         response = model.generate_content(image)
     return response.text
 
-st.set_page_config(page_title="Automated Essay Analysis", page_icon="icon.png")
+st.set_page_config(page_title="Extract Content", page_icon="icon.png")
 st.header("Automated Essay Analysis")
 
-input = "Extract the exact handwritten text"
+input = "Extract Handwritten text from PDF"
 
-uploaded_file = st.file_uploader("Choose an Image...", type=["jpg","jpeg","png"])
-image = "" 
+uploaded_file = st.file_uploader("Choose a PDF File...", type=["pdf"])
+
+l = []
+
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image,caption="Uploaded Image.",use_column_width=True)
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.read())
+        tmp_file.seek(0)
+        pdf_document = fitz.open(tmp_file.name)
+        
+        num_pages = len(pdf_document)
+        
+        for page_no in range(num_pages):
+            page = pdf_document.load_page(page_no)
+            pix = page.get_pixmap()
+            img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+            st.image(img, caption=f"Page {page_no + 1}", use_column_width=True)
+            response=get_gemini_response(input,img)
+            st.subheader("The Response is ")
+            l.append(response)
+            st.write(response)
 
-submit=st.button("Analyse")
-
-if submit:
-    response=get_gemini_response(input,image)
-    st.subheader("The Response is ")
-    st.write(response)
+for x in l:
+    print(x)
